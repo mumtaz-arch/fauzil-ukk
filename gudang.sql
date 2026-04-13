afc889a4339838e6a2aed46c6b5983d111d6617b
Fakhri Mumtaz Basyari <mumtaz.ntt@gmail.com>
2026-04-13 08:21:12 +0700

    Add files via upload

--- gudang.sql
+++ gudang.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 26, 2026 at 12:36 AM
+-- Generation Time: Apr 12, 2026 at 09:06 AM
 -- Server version: 8.0.30
--- PHP Version: 8.1.10
+-- PHP Version: 8.2.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,18 +36,19 @@ CREATE TABLE `barang` (
   `keterangan` varchar(200) NOT NULL,
   `harga_satuan` int NOT NULL,
   `harga_jual` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `barang`
 --
 
 INSERT INTO `barang` (`id_barang`, `kode_barang`, `nama_barang`, `varian_barang`, `stok_barang`, `keterangan`, `harga_satuan`, `harga_jual`) VALUES
-(12, 'ROTI001', 'roti tawar', 'Coklat', 80, '', 3000, 4500),
+(12, 'ROTI001', 'roti tawar', 'Coklat', 70, '', 3000, 4500),
 (13, 'ROTI002', 'roti tawar', 'mangga', 14, '', 5000, 7500),
-(14, 'ROTI003', 'roti tawar', 'kacang', 7, '', 2000, 3000),
+(14, 'ROTI003', 'roti tawar', 'kacang', 0, '', 2000, 3000),
 (15, 'ROTI004', 'roti tawar', 'durian', 12, '', 5000, 7500),
-(16, 'ROTI005', 'roti bulat', 'mangga', 37, '', 2000, 3000);
+(16, 'ROTI0052', 'roti sddsdbulat', 'mangga', 44, '', 2000, 31000),
+(18, 'ROTI053', 'sabagsdas', '312dfs', 112, '', 123123, 184685);
 
 -- --------------------------------------------------------
 
@@ -62,7 +63,7 @@ CREATE TABLE `transaksi` (
   `jumlah` int NOT NULL,
   `tanggal_transaksi` int NOT NULL,
   `keterangan` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `transaksi`
@@ -81,7 +82,11 @@ INSERT INTO `transaksi` (`id_transaksi`, `id_barang`, `jenis`, `jumlah`, `tangga
 (10, 14, 'masuk', 1, 20260205, ''),
 (11, 16, 'keluar', 1, 20260205, ''),
 (12, 12, 'masuk', 10, 20260207, ''),
-(13, 16, 'masuk', 20, 20260224, '');
+(13, 16, 'masuk', 20, 20260224, ''),
+(14, 18, 'masuk', 1000, 20260412, ''),
+(15, 18, 'keluar', 13000, 20260412, 'penjual'),
+(16, 16, 'masuk', 7, 20260412, ''),
+(17, 12, 'keluar', 10, 20260412, '');
 
 -- --------------------------------------------------------
 
@@ -92,15 +97,15 @@ INSERT INTO `transaksi` (`id_transaksi`, `id_barang`, `jenis`, `jumlah`, `tangga
 CREATE TABLE `user` (
   `id_user` int NOT NULL,
   `nama_user` varchar(100) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_user`, `nama_user`, `password`) VALUES
-(1, 'admin', '2b3ee5aafce01f6a9cde346809dd0558');
+(1, 'admin', '$2y$10$THWYzrQanukcHYqz4SMhDeNq5o950IBkOKC2z0p4S.o5st2eQ03GK');
 
 --
 -- Indexes for dumped tables
@@ -132,13 +137,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id_barang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_barang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `user`
